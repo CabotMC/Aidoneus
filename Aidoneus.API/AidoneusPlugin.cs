2ef322e427ac211490401f813a1d7855237175c2
Kyle Smith <kyleafmine@gmail.com>
2023-10-28 21:21:24 -0300

    Less initial commit

---
+++ Aidoneus.API/AidoneusPlugin.cs
@@ -0,0 +1,13 @@
+﻿namespace Aidoneus.API;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class AidoneusPlugin : Attribute
+{
+    public string? minVersion;
+    public AidoneusPlugin(string? minVersion = null) => this.minVersion = minVersion;
+
+    public Version AsVersion() {
+        if (minVersion == null) return new Version(0, 0, 0);
+        return new Version(minVersion);
+    }
+}
--- Aidoneus/Program.cs
+++ Aidoneus/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Reflection;
+using Aidoneus.API;
+using Discord;
+using Discord.WebSocket;
+
+namespace Aidoneus;
+public class Program {
+    static DiscordSocketClient _client;
+
+    public static void Main(string[] args) {
+        var version = Assembly.GetAssembly(typeof(Program)).GetName().Version;
+        Console.WriteLine($"Aidoneus v{version.Major}.{version.Minor}.{version.Build}");
+        var token = Environment.GetEnvironmentVariable("AIDONEUS_TOKEN");
+        if (token == null) {
+            Console.WriteLine("FATAL: AIDONEUS_TOKEN not set");
+            return;
+        }
+
+    }
+
+    public static async Task AsyncMain(string[] args, string token) {
+        _client = new DiscordSocketClient(new DiscordSocketConfig {
+            LogLevel = args.Contains("v") ? Discord.LogSeverity.Verbose : Discord.LogSeverity.Info
+        });
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        await Task.Delay(-1); 
+    }
+}
